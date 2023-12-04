68a09f83a9f8476d29a1ea19bf7894bf8208ab48
Andrew Wijaya <33770251+andrewwijaya96@users.noreply.github.com>
2023-12-04 14:21:41 +0700

    first commit

---
+++ Script/navJS.js
@@ -0,0 +1,11 @@
+function goToCatalog(){
+    window.open("catalog.html", "_self");
+}
+
+function goToInsta(){
+    window.open("https://www.instagram.com/andrewwijaya96/?hl=en");
+}
+
+function goToYt(){
+    window.open("https://www.youtube.com/channel/UCRc-tH8ZsfD3SeBJFDPFUmA");
+}
